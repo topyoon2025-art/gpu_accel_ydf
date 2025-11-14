402fb55d5be2a708514a972afc78fe3ee8a86b5e
Ariel Lubonja <ariel@cs.jhu.edu>
2025-11-14 13:34:56 -0500

    Restore all accuracy script lines

--- benchmarks/src/bash_scripts/accuracy_runtime.sh
+++ benchmarks/src/bash_scripts/accuracy_runtime.sh
@@ -10,19 +10,19 @@ logdir="benchmarks/results"
 ts=$(date +%Y%m%d_%H%M%S)              # time-stamp for uniqueness
 logfile="${logdir}/accuracy_runtime_${ts}.log"
 
-# numerical_split_type="Exact"
+numerical_split_type="Exact"
 
-# printf "\n\n--------Running all $numerical_split_type w/ $histogram_num_bins 2>&1 | tee -a "$logfile" bins-------\n\n"
+printf "\n\n--------Running all $numerical_split_type w/ $histogram_num_bins 2>&1 | tee -a "$logfile" bins-------\n\n"
 
-# ./bazel-bin/examples/train_oblique_forest --input_mode csv --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode csv --num_trees 100 --num_threads -1 --train_csv "benchmarks/data/cc18_binary_csv/task_14965_bank-marketing/repeat0_fold0_sample0_train.csv" --label_col Class --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode csv --num_trees 100 --num_threads -1 --train_csv "benchmarks/data/cc18_binary_csv/task_14952_PhishingWebsites/repeat0_fold0_sample0_train.csv" --label_col Result --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode csv --num_trees 100 --num_threads -1 --train_csv "benchmarks/data/cc18_binary_csv/task_29_credit-approval/repeat0_fold0_sample0_train.csv" --label_col class --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode csv --num_trees 100 --num_threads -1 --train_csv "benchmarks/data/cc18_binary_csv/task_167125_Internet-Advertisements/repeat0_fold0_sample0_train.csv" --label_col class --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode csv --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode csv --num_trees 100 --num_threads -1 --train_csv "benchmarks/data/cc18_binary_csv/task_14965_bank-marketing/repeat0_fold0_sample0_train.csv" --label_col Class --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode csv --num_trees 100 --num_threads -1 --train_csv "benchmarks/data/cc18_binary_csv/task_14952_PhishingWebsites/repeat0_fold0_sample0_train.csv" --label_col Result --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode csv --num_trees 100 --num_threads -1 --train_csv "benchmarks/data/cc18_binary_csv/task_29_credit-approval/repeat0_fold0_sample0_train.csv" --label_col class --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode csv --num_trees 100 --num_threads -1 --train_csv "benchmarks/data/cc18_binary_csv/task_167125_Internet-Advertisements/repeat0_fold0_sample0_train.csv" --label_col class --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
 
-# ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 10000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 100000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 10000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 100000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" 2>&1 | tee -a "$logfile"
 
 
 numerical_split_type="Equal Width"
@@ -37,7 +37,7 @@ printf "\n\n--------Running all $numerical_split_type w/ $histogram_num_bins 2>&
 
 ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 10000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
 ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 100000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
 
 
 numerical_split_type="Random"
@@ -52,7 +52,7 @@ printf "\n\n--------Running all $numerical_split_type w/ $histogram_num_bins 2>&
 
 ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 10000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
 ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 100000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
 
 
 numerical_split_type="Subsample Points"
@@ -67,7 +67,7 @@ printf "\n\n--------Running all $numerical_split_type w/ $histogram_num_bins 2>&
 
 ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 10000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
 ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 100000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
 
 
 numerical_split_type="Subsample Histogram"
@@ -82,7 +82,7 @@ printf "\n\n--------Running all $numerical_split_type w/ $histogram_num_bins 2>&
 
 ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 10000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
 ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 100000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
 
 
 numerical_split_type="Dynamic Equal Width Histogram"
@@ -112,4 +112,4 @@ printf "\n\n--------Running all $numerical_split_type w/ $histogram_num_bins 2>&
 
 ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 10000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
 ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 100000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
-# ./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
+./bazel-bin/examples/train_oblique_forest --input_mode trunk --rows 1000000 --num_trees 100 --num_threads -1 --numerical_split_type "$numerical_split_type" --histogram_num_bins $histogram_num_bins 2>&1 | tee -a "$logfile"
